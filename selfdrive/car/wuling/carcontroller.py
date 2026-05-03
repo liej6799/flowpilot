3c0da1c146b8a1bcc23adbb619bf0c120892a1e2
Joes Lie <52598803+liej6799@users.noreply.github.com>
2026-05-03 13:16:37 +0700

    Revert "use original logic"
    This reverts commit f432a3ed749a83bd5d87cc018282eea7fc004392.

--- selfdrive/car/wuling/carcontroller.py
+++ selfdrive/car/wuling/carcontroller.py
@@ -7,25 +7,25 @@ from selfdrive.car import apply_driver_steer_torque_limits, apply_std_steer_angl
 from selfdrive.car.wuling import wulingcan
 from selfdrive.car.wuling.values import DBC, CanBus, PREGLOBAL_CARS, CarControllerParams
 from common.numpy_fast import clip
-
+from common.logger import sLogger
 LongCtrlState = car.CarControl.Actuators.LongControlState
 
 class CarController:
   def __init__(self, dbc_name, CP, VM):
     self.CP = CP
-    
+
     self.lka_steering_cmd_counter_last = -1
     self.apply_angle_last = 0
-    
+
     self.frame = 0
 
     self.params = CarControllerParams(self.CP)
-    
+
     self.packer_pt = CANPacker(DBC[self.CP.carFingerprint]['pt'])
-    
+
   def update(self, CC, CS, now_nanos):
     P = self.params
-    
+
     # Send CAN commands.
     can_sends = []
     actuators = CC.actuators
@@ -33,13 +33,12 @@ class CarController:
     hud_alert = hud_control.visualAlert
     hud_v_cruise = hud_control.setSpeed
 
-    
+
     if CC.latActive:
       apply_angle = apply_std_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgo, CarControllerParams)
     else:
       apply_angle = CS.out.steeringAngleDeg
-      
-     
+
     if CC.longActive:
       apply_gas = int(round(interp(actuators.accel, P.GAS_LOOKUP_BP, P.GAS_LOOKUP_V)))
       brake_value = int(round(interp(actuators.accel, P.BRAKE_LOOKUP_BP, P.BRAKE_LOOKUP_V)))
@@ -47,9 +46,9 @@ class CarController:
       apply_stop = 0
       apply_gas = 1696
       brake_value = 255
-    
+
     self.apply_angle_last = apply_angle
-    
+
     if CS.lka_steering_cmd_counter != self.lka_steering_cmd_counter_last:
       self.lka_steering_cmd_counter_last = CS.lka_steering_cmd_counter
     elif (self.frame % P.STEER_STEP) == 0:
@@ -57,14 +56,15 @@ class CarController:
       acc_enabled = CC.longActive
       apply_stop = actuators.longControlState == LongCtrlState.stopping
       apply_start = actuators.longControlState == LongCtrlState.starting
-      
+
       idx = (self.frame/2) % 4
 
-      
-      can_sends.append(wulingcan.create_steering_control(self.packer_pt, apply_angle, idx, lkas_enabled))
-      can_sends.append(wulingcan.create_brake_command(self.packer_pt, apply_stop, idx, brake_value))
-      can_sends.append(wulingcan.create_gas_command(self.packer_pt, idx, acc_enabled, apply_start, apply_gas))
 
+      can_sends.append(wulingcan.create_steering_control(self.packer_pt, apply_angle, idx, lkas_enabled))
+      #can_sends.append(wulingcan.create_brake_command(self.packer_pt, acc_enabled, idx, brake_value))
+      can_sends.append(wulingcan.create_gas_command(self.packer_pt, idx, acc_enabled, apply_gas))
+
+    sLogger.Send("0all set")
     if (self.frame % P.HUD_STEP) == 0:
       set_speed = int(round(hud_v_cruise * CV.MS_TO_KPH))
       idx = (self.frame/5) % 4
@@ -72,11 +72,11 @@ class CarController:
       can_sends.append(wulingcan.create_acc_hud_control(self.packer_pt, idx, cc_enabled, set_speed))
       can_sends.append(wulingcan.create_lkas_hud_control(self.packer_pt, idx, cc_enabled))
 
-       
+
     new_actuators = actuators.copy()
     new_actuators.steeringAngleDeg = apply_angle
     new_actuators.brake = brake_value
     new_actuators.gas = apply_gas
-    
+
     self.frame += 1
     return new_actuators, can_sends
--- selfdrive/car/wuling/wulingcan.py
+++ selfdrive/car/wuling/wulingcan.py
@@ -14,16 +14,16 @@ def create_steering_control(packer, apply_steer, idx, steer_req):
         # "STEER_TORQUE_CMD": -apply_steer,
         # "SET_ME_X0": 0x00,
         # "STEER_REQUEST": steer_req,
-        
+
         "ACTIVE": 0x64, # Always active 
         "ACTIVE_2": 0x64 if steer_req else 0,
         "STEER_LOCK": 1 if steer_req else 0,
         "COUNTER": idx,
         "STEER_ANGLE_CMD": -apply_steer
   }
-  
+
   values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
-  
+
   dat = packer.make_can_msg("STEERING_LKA", 0, values)[2]
 
   crc = wuling_checksum(dat[:-1])
@@ -31,23 +31,23 @@ def create_steering_control(packer, apply_steer, idx, steer_req):
 
   return packer.make_can_msg("STEERING_LKA", 0, values)
 
-def create_gas_command(packer, idx, acc_req, apply_start, throttle):
+def create_gas_command(packer, idx, apply_start, throttle):
   values = {
     "ALWAYS_ON_1": 5,
     "ALWAYS_ON_2": 0x78,
-    "ACC_ACTIVE_1": 32 if acc_req else 0,
-    "ACC_ACTIVE_2": 1 if acc_req else 0,
-    
-    "GAS_ACTIVE_2": 1 if acc_req and throttle != 1696 else 0, # change this later
-    "INV_GAS_ACTIVE_2": 1 if acc_req and throttle == 1696 else 0, #change this later
+    "ACC_ACTIVE_1": 32 if apply_start else 0,
+    "ACC_ACTIVE_2": 1 if apply_start else 0,
+
+    "GAS_ACTIVE_2": 1 if apply_start and throttle != 1696 else 0, # change this later
+    "INV_GAS_ACTIVE_2": 1 if apply_start and throttle == 1696 else 0, #change this later
 
     "COUNTER": idx,
-    "GAS_ACTIVE": 4 if acc_req and throttle != 1696 else 0, # need to check is it based on throttle.
+    "GAS_ACTIVE": 4 if apply_start and throttle != 1696 else 0, # need to check is it based on throttle.
     "GAS_THROTTLE": throttle # need to change later
   }
-  
+
   values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
-  
+
   dat = packer.make_can_msg("AccStatus", 0, values)[2]
 
   crc = wuling_checksum(dat[:-1])
@@ -57,16 +57,15 @@ def create_gas_command(packer, idx, acc_req, apply_start, throttle):
 
 
 def create_brake_command(packer, apply_brake, idx, brake_value):
-  print(brake_value)
   values = {
-    "BRAKE_CMD": 15 if apply_brake < 200 else 0,
-    "BRAKE_CMD_2": 16 if apply_brake < 200 else 0,
+    "BRAKE_CMD": 15 if apply_brake else 0,
+    "BRAKE_CMD_2": 16 if apply_brake else 0,
     "COUNTER": idx, # 0 - 255
     "BRAKE_VAL": brake_value,
   }
-  
+
   values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
-  
+
   dat = packer.make_can_msg("BrakeCMD", 0, values)[2]
 
   crc = wuling_checksum(dat[:-1])
@@ -86,9 +85,9 @@ def create_acc_hud_control(packer, idx, acc_req, target_speed_kph):
     "ACC_ENABLED_3": 1 if acc_req else 0, 
     "COUNTER": idx,
   }
-  
+
   values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
-  
+
   dat = packer.make_can_msg("ASCMActiveCruiseControlStatus", 0, values)[2]
 
   crc = wuling_checksum(dat[:-1])
@@ -103,14 +102,12 @@ def create_lkas_hud_control(packer, idx, acc_req):
     "LKA_ACTIVE": 1 if acc_req else 0, 
     "COUNTER": idx,
   }
-  
+
   values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
-  
+
   dat = packer.make_can_msg("LkasHud", 0, values)[2]
 
   crc = wuling_checksum(dat[:-1])
   values["CHECKSUM"] = crc
 
   return packer.make_can_msg("LkasHud", 0, values)
-
-  
